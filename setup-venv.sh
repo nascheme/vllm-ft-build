44a5c68dcd55c42de124ba56edc2e3d8c1513611
Neil Schemenauer <nas@arctrix.com>
2026-04-03 17:48:09 -0700

    Add "cuda-nightly" option to setup-venv.sh.

--- setup-venv.sh
+++ setup-venv.sh
@@ -2,11 +2,13 @@
 #
 # Create venv and install Python packages.
 #
-# Usage ./setup-venv.sh [ cpu | cuda | rocm ]
+# Usage ./setup-venv.sh [ cpu | cuda | cuda-nightly | rocm ]
 
 set -eu
 
 dev=$1
+pre=""
+torch_req=requirements/torch.txt
 
 case $dev in
     cpu)
@@ -18,11 +20,16 @@ case $dev in
     cuda)
         url=https://download.pytorch.org/whl/cu128
         ;;
+    cuda-nightly)
+        url=https://download.pytorch.org/whl/nightly/cu128
+        pre="--pre"
+        torch_req=requirements/torch-nightly.txt
+        ;;
     *)
         echo "Unknown compute device $dev"
         exit 1
 esac
 
 uv venv --python=3.14t
-uv pip install -r requirements/torch.txt --extra-index-url "$url"
+uv pip install $pre -r "$torch_req" --extra-index-url "$url"
 uv pip install -r requirements/common.txt
